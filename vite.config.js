f7230a28a541aed56bf9721f239b10099a4e82c5
UrvishAhir1 <urvishahir7@gmail.com>
2025-04-14 16:56:46 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  const isServer = ssrBuild === true
+
+  return {
+    plugins: [react()],
+    build: {
+      ssr: isServer,
+      outDir: isServer ? 'dist/server' : 'dist/client',
+      rollupOptions: {
+        input: isServer ? './src/entry-server.jsx' : './index.html',
+      },
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  }
 })
